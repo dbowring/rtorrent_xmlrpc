39d324c003c8a7b8967a00d124de3a57dab6b240
Daniel Bowring <github@danielb.codes>
2018-03-11 17:29:38 +0900

    Update: add setup scripts

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='rtorrent_xmlrpc',
+    packages=['rtorrent_xmlrpc'],
+    version='0.0.2',
+    description='Library for direct communication with rtorrent\'s xmlrpc via scgi',
+    author='Daniel Bowring',
+    author_email='contact@danielb.codes',
+    url='https://github.com/dbowring/rtorrent_xmlrpc',
+    download_url='https://github.com/dbowring/rtorrent_xmlrpc/archive/0.0.2.tar.gz',
+    keywords=['rtorrent', 'xmlrpc', 'scgi'],
+    classifiers=[],
+)
+
