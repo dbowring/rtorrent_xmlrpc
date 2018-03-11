89c63a6456f04b688b4e3970af4019135e9d90d9
Daniel Bowring <github@danielb.codes>
2018-03-11 17:51:47 +0900

    Update version to 0.0.3

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='rtorrent_xmlrpc',
     packages=['rtorrent_xmlrpc'],
-    version='0.0.2',
+    version='0.0.3',
     description='Library for direct communication with rtorrent\'s xmlrpc via scgi',
     author='Daniel Bowring',
     author_email='contact@danielb.codes',
     url='https://github.com/dbowring/rtorrent_xmlrpc',
-    download_url='https://github.com/dbowring/rtorrent_xmlrpc/archive/0.0.2.tar.gz',
+    download_url='https://github.com/dbowring/rtorrent_xmlrpc/archive/0.0.3.tar.gz',
     keywords=['rtorrent', 'xmlrpc', 'scgi'],
     classifiers=[],
 )
